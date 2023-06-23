77de65ba5f9668357ec510a165de0b089b371431
Yanina-t <vyaninat@gmail.com>
2023-06-23 17:36:49 +0200

    add def title_letter and docstring local

--- str_func.py
+++ str_func.py
@@ -1,3 +1,15 @@
 def capital_letter():
+    """
+    docstring 1 local
+    :return:
+    """
     a = input()
     return a.upper()
+
+def title_letter():
+    """
+    docstring 2 local
+    :return:
+    """
+    a = input()
+    return a.title()
